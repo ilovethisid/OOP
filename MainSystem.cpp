9417b9a31aa2c799266423e50461c75c7c188f1a
bdk <namebdk@naver.com>
2018-11-28 02:53:19 +0900

    Add files via upload

--- MainSystem.cpp
+++ MainSystem.cpp
@@ -3,43 +3,35 @@
 
 #include "glut.h"
 
+#define FPS 30
+#define gravity 3   // 중력
+#define jumpspeed 7 // 점프하는 순간 속력
+#define gameover 0
+
 using namespace player;
 
-void Map1();
-void keyListener(int key, int x, int y);
-void changeSize(int w, int h);
+int initial_time = time(NULL);
+int final_time;
+int frame_count = 0;
+int Top_angle;
+int Down_angle;
 
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(glutGet(GLUT_SCREEN_WIDTH) / 2 - WINWIDTH / 2, glutGet(GLUT_SCREEN_HEIGHT) / 2 - WINHEIGHT / 2); // 실행되는 시작 위치
 	glutInitWindowSize(WINWIDTH, WINHEIGHT);     // 생성되는 윈도우 크기
 	glutCreateWindow("Test Game"); // 설정한 이름으로 윈도우를 생성함
-	glutDisplayFunc(Map1);
-	glutReshapeFunc(changeSize); // 창의 크기가 변해도 비율은 변하지 않게 해준다.
+
+	glutDisplayFunc(display);
+	glutReshapeFunc(changeSize);
+	glutTimerFunc((1000 / FPS), idle, 0);
 
 	glutSpecialFunc(keyListener); // 키보드 특수키 입력 처리
-
-	glEnable(GL_DEPTH_TEST); // 깊이 검사가 가능하도록 설정한다.
 	glutMainLoop();
-
 	return 0;
 }
-
-void Map1()
-{
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // 버퍼 지우기
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glRectf(-1.5, 1.5, 1.5, -1.5);
-
-	glClear(GL_DEPTH_BUFFER_BIT); // 깊이 버퍼 지우기
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glRectf(playerX1, playerY1, playerX2, playerY2);
-
-	glFlush();
-}
-
 
 void changeSize(int w, int h)
 {
@@ -62,6 +54,30 @@ void changeSize(int w, int h)
 	gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, -1.0, 0.0f, 1.0f, 0.0f);
 }
 
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // 버퍼 지우기
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glRectf(-1.5, 1.5, 1.5, -1.5);
+
+	glClear(GL_DEPTH_BUFFER_BIT); // 깊이 버퍼 지우기
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glRectf(playerX1, playerY1, playerX2, playerY2);
+
+	glutSwapBuffers();
+
+	frame_count++;
+	final_time = time(NULL);
+	if (final_time - initial_time > 0)
+	{
+		cout << "FPS : " << frame_count / (final_time - initial_time) << endl;
+		frame_count = 0;
+		initial_time = final_time;
+	}
+
+	glFlush(); // 쌓여있는 명령 지움
+}
+
 void keyListener(int key, int x, int y)
 {
 	//키보드 방향에 따른 위치이동
@@ -75,7 +91,7 @@ void keyListener(int key, int x, int y)
 		playerX1 += WALKSPD;
 		playerX2 += WALKSPD;
 	}
-	
+
 	if (GetKeyState(VK_UP) < 0)
 	{
 		playerY1 += WALKSPD;
@@ -88,4 +104,25 @@ void keyListener(int key, int x, int y)
 	}
 
 	glutPostRedisplay();
+}
+
+void idle(int) // 이중버퍼에 들어갈 함수
+{
+	glutPostRedisplay();
+	glutTimerFunc((1000 / FPS), idle, 0);
+	if (playerY2 < Down_angle)
+	{
+		Top_angle -= 0.05;
+		Down_angle -= 0.05;
+		glTranslatef(0.0, -0.05, 0.0);
+	}
+	if (playerY1 > Top_angle)
+	{
+		Top_angle += 0.05;
+		Down_angle += 0.05;
+		glTranslatef(0.0, 0.05, 0.0);
+
+	}
+	playerY1 -= 0.05;
+	playerY2 -= 0.05;
 }
--- glut.h
+++ glut.h
@@ -1,13 +1,16 @@
 #pragma once
 #pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
 
-#include <gl/glut.h>
+#include <GL/glut.h>
 #include <cstdlib>
-#include <ctime>
+#include <time.h>
+#include <iostream>
 
 #define WALKSPD 0.05
 #define WINWIDTH 800
 #define WINHEIGHT 600
+
+using namespace std;
 
 namespace player
 {
@@ -17,3 +20,8 @@ namespace player
 	float playerX2 = 0.1;
 	float playerY2 = -0.1;
 }
+
+void keyListener(int key, int x, int y);
+void changeSize(int w, int h);
+void idle(int);
+void display();
