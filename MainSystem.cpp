91b46f5cecaee842e81378ad94faf5148dd65fac
bdk <namebdk@naver.com>
2018-11-30 20:18:48 +0900

    Add files via upload
    점프, 중력 삽입

--- MainSystem.cpp
+++ MainSystem.cpp
@@ -1,20 +1,23 @@
-#include <Windows.h>
-#include <WinUser.h>
-
 #include "glut.h"
 
-using namespace player;
 using namespace angle;
-
-int initial_time = clock();
-int final_time;
-int frame_count = 0;
+using namespace player;
+using namespace mouse;
+using namespace speed;
 
 int main(int argc, char** argv)
+{
+	glut GLUT;
+	GLUT.startFramework(argc, argv);
+	return 0;
+}
+
+void glut::startFramework(int argc, char** argv)
 {
 	Map Map;
 	mapsetting(Map);
 
+	// Init
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(glutGet(GLUT_SCREEN_WIDTH) / 2 - WINWIDTH / 2, glutGet(GLUT_SCREEN_HEIGHT) / 2 - WINHEIGHT / 2); // 실행되는 시작 위치
@@ -26,11 +29,12 @@ int main(int argc, char** argv)
 	glutTimerFunc((1000 / FPS), idle, 0);
 
 	glutSpecialFunc(keyListener); // 키보드 특수키 입력 처리
+	glutPassiveMotionFunc(mousePassiveMotion);
+	glutMouseFunc(mouseClick);
 	glutMainLoop();
-	return 0;
 }
 
-void changeSize(int w, int h)
+void glut::changeSize(int w, int h)
 {
 	// 창이 아주 작을 때, 0으로 나누는 것을 예방
 	if (h == 0)
@@ -51,14 +55,14 @@ void changeSize(int w, int h)
 	gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, -1.0, 0.0f, 1.0f, 0.0f);
 }
 
-void display()
+void glut::display()
 {
-	
+
 	// 배경 그리기
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // 버퍼 지우기
 	glColor3f(0.0f, 1.0f, 0.0f);
 	glRectf(-1, 1.4, 1, -0.6);
-	
+
 	// 맵 그리기
 	for (int i = 0; i < MapLength; i++)
 	{
@@ -67,7 +71,7 @@ void display()
 			if (MAP[i][j] == 1)
 			{
 				glColor3f(0.0f, 0.0f, 1.0f);
-				glRectf(LUcornerX+0.2*(float)j,LUcornerY-0.2*(float)i,RDcornerX+0.2*(float)j,RDcornerY-0.2*(float)i);
+				glRectf(LUcornerX + 0.2*(float)j, LUcornerY - 0.2*(float)i, RDcornerX + 0.2*(float)j, RDcornerY - 0.2*(float)i);
 			}
 		}
 	}
@@ -78,13 +82,71 @@ void display()
 
 	glutSwapBuffers();
 
-	frame_count++;
-	final_time = clock();
-
 	glFlush(); // 쌓여있는 명령 지움
 }
 
-void keyListener(int key, int x, int y)
+void glut::idle(int) // 이중버퍼에 들어갈 함수
+{
+	if (V_jump != 0.0) // 땅에 닿았을 때 (수직 속도가 0일 때)
+	{
+		playerY1 += V_jump;
+		playerY2 += V_jump;
+		V_jump -= gravity;
+	}
+	
+	if (V_horizon > 0.0)
+	{
+		playerX1 += V_horizon;
+		playerX2 += V_horizon;
+		//V_horizon -= friction;
+	}
+	else if (V_horizon < 0.0)
+	{
+		playerX1 += V_horizon;
+		playerX2 += V_horizon;
+		//V_horizon += friction;
+	}
+		
+	if (playerY2 < Down_angle)
+	{
+		Top_angle -= anglemovespeed;
+		Down_angle -= anglemovespeed;
+		glTranslatef(0.0, anglemovespeed, 0.0);
+	}
+	else if (playerY2 > Top_angle)
+	{
+		Top_angle += anglemovespeed;
+		Down_angle += anglemovespeed;
+		glTranslatef(0.0, (-1 * anglemovespeed), 0.0);
+
+	}
+	else
+	{
+		// 화면 고정
+	}
+	glutPostRedisplay();
+	glutTimerFunc((1000 / FPS), idle, 0);
+}
+
+void glut::mapsetting(Map Map)
+{
+	for (int i = 0; i < MapLength; i++)
+	{
+		for (int j = 0; j < MapWidth; j++)
+		{
+			if (Map.getXY(i, j) == 1)
+			{
+				MAP[i][j] = 1;
+			}
+			else
+			{
+				MAP[i][j] = 0;
+			}
+		}
+	}
+}
+
+void glut::keyListener(int key, int x, int y)
 {
 	//키보드 방향에 따른 위치이동
 	if (GetKeyState(VK_LEFT) < 0)
@@ -112,43 +174,43 @@ void keyListener(int key, int x, int y)
 	glutPostRedisplay();
 }
 
-void idle(int) // 이중버퍼에 들어갈 함수
+void glut::mouseActiveMotion(int x, int y)
 {
-	if (playerY2 < Down_angle)
-	{
-		Top_angle -= anglemovespeed;
-		Down_angle -= anglemovespeed;
-		glTranslatef(0.0, anglemovespeed, 0.0);
-	}
-	else if (playerY1 > Top_angle)
-	{
-		Top_angle += anglemovespeed;
-		Down_angle += anglemovespeed;
-		glTranslatef(0.0, (-1 * anglemovespeed), 0.0);
-
-	}
-	else
-	{
-		// 화면 고정
-	}
-	glutPostRedisplay();
-	glutTimerFunc((1000 / FPS), idle, 0);
+	
 }
 
-void mapsetting(Map Map)
+void glut::mousePassiveMotion(int x, int y)
 {
-	for (int i = 0; i < MapLength; i++)
+	/*
+	specialKey = glutGetModifiers();
+	if (x > playerX1 && x < playerX2 && y < playerY1 && y > playerY2)
 	{
-		for (int j = 0; j < MapWidth; j++)
-		{
-			if (Map.getXY(i, j) == 1)
-			{
-				MAP[i][j] = 1;
-			}
-			else
-			{
-				MAP[i][j] = 0;
-			}
-		}
+		ClickX1 = playerX1;
+		ClickY1 = playerY1;
+		ClickX2 = playerX2;
+		ClickY2 = playerY2;
+		glColor3f(0.0f, 1.0f, 0.0f);
+		glRectf(ClickX1, ClickY1, ClickX2, ClickY2);
 	}
+	
+	glutPostRedisplay();
+	*/
+	
+}
+
+void glut::mouseClick(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		mouseX1 = x;
+		mouseY1 = y;
+	}
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_UP)
+	{
+		mouseX2 = x;
+		mouseY2 = y;
+		V_horizon += (mouseX1 - mouseX2)*(0.0001);
+		V_jump += (mouseY1 - mouseY2)*(-0.0001);
+	}
+	glutPostRedisplay();
 }
--- glut.h
+++ glut.h
@@ -2,25 +2,32 @@
 #pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
 
 #include <GL/glut.h>
-#include <cstdlib>
-#include <time.h>
-#include <iostream>
+#include <Windows.h>
+#include <WinUser.h>
 #include "Map.h"
+//#include "Game.h"
 
 #define WALKSPD 0.5
+#define gravity 0.0002   // 중력
+#define friction 0.0002  // 마찰력
 #define WINWIDTH 800
 #define WINHEIGHT 600
 #define FPS 80
-#define gravity 3   // 중력
-#define jumpspeed 7 // 점프하는 순간 속력
 #define gameover 0
 #define anglemovespeed 0.03 // 화면 움직임 속도
+#define jumpmovespeed 0.03  // 점프 움직임 속도
 #define LUcornerX -1
 #define LUcornerY 1.4
 #define RDcornerX -0.8
 #define RDcornerY 1.2
 
 using namespace std;
+
+namespace angle
+{
+	float Top_angle = 0;
+	float Down_angle = -1;
+}
 
 namespace player
 {
@@ -30,16 +37,33 @@ namespace player
 	float playerX2 = 0.1;
 	float playerY2 = -0.1;
 }
-namespace angle
+
+namespace mouse
 {
-	float Top_angle = 1;
-	float Down_angle = -1;
+	int mouseX1;
+	int mouseY1;
+	int mouseX2;
+	int mouseY2;
+}
+
+namespace speed
+{
+	float V_jump = 0.0;
+	float V_horizon = 0.0;
 }
 
 int MAP[10][10];
 
-void keyListener(int key, int x, int y);
-void changeSize(int w, int h);
-void idle(int);
-void display();
-void mapsetting(Map);
+class glut
+{
+public:
+	static void mouseClick(int button, int motion, int x, int y); // 마우스 클릭 여부
+	static void mousePassiveMotion(int x, int y);                 // 마우스 클릭 안됐을 때 마우스 위치
+	static void mouseActiveMotion(int x, int y);                  // 마우스 클릭하면서 움직이는 마우스 위치
+	static void keyListener(int key, int x, int y);
+	static void changeSize(int w, int h);
+	static void idle(int);
+	static void display();
+	static void mapsetting(Map);
+	void startFramework(int argc, char** argv);
+};
